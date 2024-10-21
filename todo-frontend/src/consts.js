75156c515364b34fcef41cb8e9a99acc898f9e8e
George <george.alexander.saav@gmail.com>
2024-10-21 15:26:23 +0200

    backend URL provided

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -1,6 +1,6 @@
 import "react";
 
-const url = import.meta.env.VITE_ENDPOINT_URL; //|| "http://localhost:5168";
-
+const url = "https://georgewebapp.azurewebsites.net/"; //|| "http://localhost:5168";
+//const url = "http://localhost:5173/"
 // ensure trailing / is removed
 export const API_URL = url.replace(/\/$/, "");
