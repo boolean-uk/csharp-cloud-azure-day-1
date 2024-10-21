011323e96060cf247e2206f859c2da0536bb6a43
Felix <felix.mathiasson99@gmail.com>
2024-10-21 14:09:35 +0200

    Commit made from VS Code Azure Static Web Apps

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -1,6 +1,6 @@
 import "react";
 
-const url = import.meta.env.VITE_ENDPOINT_URL; //|| "http://localhost:5168";
+const url = "https://felix-web-app-thingy.azurewebsites.net";// import.meta.env.VITE_ENDPOINT_URL; //|| "http://localhost:5168";
 
 // ensure trailing / is removed
 export const API_URL = url.replace(/\/$/, "");
