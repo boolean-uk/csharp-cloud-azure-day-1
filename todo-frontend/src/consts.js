ae75ae3d9923879a79dde738208e757b10fb9d57
DanielMjosRoli <daniel.mrfush@gmail.com>
2024-10-21 14:59:37 +0200

    test

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -1,6 +1,6 @@
 import "react";
 
-const url = "https://espensl-app.azurewebsites.net/"; //|| "http://localhost:5168";
+const url = "https://espensl-app.azurewebsites.net"; //|| "http://localhost:5168";
 
 // ensure trailing / is removed
 export const API_URL = url.replace(/\/$/, "");
