b344719b2ca28c455b76b8d4d4a0a9eb408c0d1e
John Ferdie Abueg <john.abueg@gmail.com>
2024-10-21 15:18:46 +0200

    backend

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -1,6 +1,7 @@
 import "react";
 
-const url = import.meta.env.VITE_ENDPOINT_URL; //|| "http://localhost:5168";
+// const url = import.meta.env.VITE_ENDPOINT_URL; //|| "http://localhost:5168";
+const url = 'https://johnfa-backend-day-1.azurewebsites.net/'
 
 // ensure trailing / is removed
 export const API_URL = url.replace(/\/$/, "");
