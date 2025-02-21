55699ea94d81cef9afc4ed1241a90f77ccb8efc3
Noah Ekse <nolis1000@hotmail.com>
2025-02-21 10:19:14 +0100

    fix v2

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -1,6 +1,6 @@
 import "react";
 
-const url = import.meta.env.VITE_ENDPOINT_URL; //|| "http://localhost:5168";
+const url = import.meta.env.VITE_ENDPOINT_URL;
 
 // ensure trailing / is removed
 export const API_URL = url.replace(/\/$/, "");
