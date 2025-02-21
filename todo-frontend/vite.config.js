15276944620527208515a1e4eb9be6f2f5649f19
Nikolai L. Børseth <nikolailb1999@gmail.com>
2025-02-21 10:44:14 +0100

    I gave up

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "import.meta.env.VITE_API_URL": "http://localhost:3001",
-  },
 });
