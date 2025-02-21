85da226d38fc8b131b60b3ce62eb8cb6d56a3202
Kristoffer Blucher <kristofferblucher@gmail.com>
2025-02-21 09:57:18 +0100

    initial

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -3,4 +3,4 @@ import "react";
 const url = import.meta.env.VITE_ENDPOINT_URL; //|| "http://localhost:5168";
 
 // ensure trailing / is removed
-export const API_URL = url.replace(/\/$/, "");
+// export const API_URL = url.replace(/\/$/, "");
