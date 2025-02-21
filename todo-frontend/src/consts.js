dc1a785d9fc53f216b75d3d4fe765b96a0075971
Enock97 <enockladu@hotmail.com>
2025-02-21 11:06:30 +0100

    + commit 2

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -2,5 +2,5 @@ import "react";
 
 const url = import.meta.env.VITE_ENDPOINT_URL; //|| "http://localhost:5168";
 
-// ensure trailing / is removed B
+// ensure trailing / is removed BB
 export const API_URL = url;
