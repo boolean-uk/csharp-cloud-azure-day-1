6599317734c7669e04d707f3134cdfad769952c4
Martin Lind <marlin00.Work@gmail.com>
2025-02-21 12:58:03 +0100

    maybe fixed error

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -3,4 +3,11 @@ import "react";
 const url = import.meta.env.VITE_ENDPOINT_URL; //|| "http://localhost:5168";
 
 // ensure trailing / is removed
-export const API_URL = url.replace(/\/$/, "");
+let test;
+if(url)
+    test = url.replace(/\/$/, "");
+else
+    test = "";
+
+export const API_URL = test;
+
