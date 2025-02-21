e228322012f943236b3899cf7ddfaab35c3725ef
Noah Ekse <nolis1000@hotmail.com>
2025-02-21 10:59:08 +0100

    fix

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -2,5 +2,5 @@ import "react";
 
 const url = window.env?.VITE_ENDPOINT_URL || "https://default-api.example.com";
 
-// ensure trailing / is removed
+// ensure trailing / is removed 
 export const API_URL = url.replace(/\/$/, "");
