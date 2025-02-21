d36468587d777fcb480c781aeeab703a788bdf4e
Nikolai L. Børseth <nikolailb1999@gmail.com>
2025-02-21 10:41:43 +0100

    One more try

--- todo-frontend/vite.config.js
+++ todo-frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   define: {
     "import.meta.env.VITE_API_URL":
-      JSON.stringify(process.env.VITE_API_URL) || "http://localhost:3001",
+      JSON.stringify(process.env.VITE_API_URL) ?? "http://localhost:3001",
   },
 });
