ce194d38578984d2c3e5b65e48dbcae084a067f7
Jone <64496635+JoneTheBuilder@users.noreply.github.com>
2025-02-21 13:02:48 +0100

    Update

--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -2,11 +2,9 @@ using exercise.wwwapi.EndPoints;
 using exercise.wwwapi.Models;
 using exercise.wwwapi.Repository;
 
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
-// Add CORS service with policy that allows any origin
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
@@ -17,18 +15,14 @@ builder.Services.AddCors(options =>
     });
 });
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IDatabaseRepository<Todo>, DatabaseRepository<Todo>>();
 
 var app = builder.Build();
 
-// Use the CORS policy
 app.UseCors("AllowAll");
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -39,3 +33,5 @@ app.ConfigureTodoAPI();
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
+
+// Jone Hjorteland
